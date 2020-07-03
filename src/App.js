d90f915b07d14a947f9a9e05f3a01f0223bfb9a9
Sameen Shakeel <49653224+SameenShakeel@users.noreply.github.com>
2020-07-03 22:02:52 +0500

    Implemented Fetch Data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [repos, setRepos] = useState([{}]);
+
+  useEffect( () => {
+
+    async function getRepos() {
+      const response = await fetch("https://api.github.com/users/sameenshakeel/repos")
+      const data = await response.json();
+      console.log(data);
+      setRepos(data)
+    }
+    getRepos();
+
+  },[])
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="App">
+      <h1>You are seeing all repositories</h1>
+      <ul>
+        {repos.map((repoObj, ind) => {
+          return (<li key={ind}>{repoObj.name}</li>)
+        })}
+      </ul>
     </div>
   );
 }
